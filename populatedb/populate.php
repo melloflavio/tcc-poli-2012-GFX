2d772a508cd10fd6cadc10ac23bf698e5ee6605c
cymbg1 <cymbaum@gmail.com>
2012-11-10 23:10:43 -0300

    Script PHP para popular DB
    Mudar os parâmetros de conexão do BD no arquivo populate.php antes de
    rodar populate.php no Wamp. Pressupoe a existencia das tabela "medidas"

---
+++ populatedb/populate.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+<title>Populate measures table</title>
+</head>
+<body>
+<?php
+
+$timestamp = date("U", mktime(4, 30, 0, 8, 21, 2012));
+$currenttime=time();
+$potencia = 30;
+$posto = 0;
+$login = "situationmmike";
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'php_login');
+//Connect to mysql server
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if(!$link) {
+	die('Failed to connect to server: ' . mysql_error());
+}
+
+//Select database
+$db = mysql_select_db(DB_DATABASE);
+if(!$db) {
+	die("Unable to select database");
+}
+
+// Floating point random number function
+function fprand($intMin,$intMax,$intDecimals) {
+	if($intDecimals) {
+		$intPowerTen=pow(10,$intDecimals);
+		return rand($intMin,$intMax*$intPowerTen)/$intPowerTen;
+	}
+	else
+	return rand($intMin,$intMax);
+}
+
+//geração da curva de demanda
+function generatevalue ($timestamp,$oldvalue){
+	$now = date("Gi",$timestamp); // gera hora atual
+	//$factor = 1 + fprand(0,0.15,3); //gera fator randomico para macroeventos
+	if ($oldvalue == 0){
+		$oldvalue = 30 * (1 + fprand(0,0.2,3) - 0.10);
+	}
+
+	if ($now > 000 && $now <= 200){
+		$potencia = $oldvalue * (1 + fprand(0.05,0.10,3) - 0.10); // DEcrescimento entre 5% e %10
+	}
+	elseif ($now > 200 && $now <= 430)
+	{
+		$potencia = 20*(1 + fprand(0.87,1.13,3)-0.65); // +-13%
+	}
+	elseif ($now > 430 && $now <= 600)
+	{
+		$potencia = 25*( 1+ fprand(0.87,1.13,3)-0.65); // +-13%
+	}
+	elseif ($now > 600 && $now <= 830)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.1,0.15,3)); // crescimento entre 10% e %15
+	}
+	elseif ($now > 830 && $now <= 900)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.1,0.15,3) - 0.015); // DEcrescimento entre 10% e %15
+	}
+	elseif ($now > 900 && $now <= 1200)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.15,0.20,3)); // crescimento entre 15% e %20
+	}
+	elseif ($now > 1200 && $now <= 1600)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.10,0.15,3) - 0.15); // DEcrescimento entre 10% e %15
+	}
+	elseif ($now > 1600 && $now <= 1830)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.15,0.27,3)); // crescimento entre 15% e %27
+	}
+	elseif ($now > 1830 && $now <= 2359)
+	{
+		$potencia = $oldvalue * (1 + fprand(0.1,0.15,3) - 0.15); // DEcrescimento entre 10% e %15
+	}
+	else
+	{
+		$potencia = $oldvalue * (1 + fprand(0.10,0.15,3) - 0.15); // DEcrescimento entre 10% e %15
+	}
+	return $potencia;
+}
+
+echo "<p>Initial UTC: ".$timestamp."</p><p>Final UTC: ".$currenttime."</p>";
+
+
+while ($timestamp <= $currenttime){
+	//generate new power value
+	$potencia = generatevalue($timestamp,$potencia);
+	//insert into DB
+	$qry = "INSERT INTO medidas(login,timestamp,potencia,posto) VALUES('$login','$timestamp','$potencia','$posto')";
+	$result = @mysql_query($qry);
+
+	//increment time in 15 mins
+	$timestamp = $timestamp + 60*15;
+	echo date(DATE_RFC822,$timestamp)."  ".$potencia."<br/>";
+}
+?>
+
+</body>
+</html>
---
+++ populatedb/test.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>My First PHP Page</title>
+</head>
+<body>
+<?php
+$timestamp = //unix time stamp
+$hour = 0;
+$minute = 0;
+$second = 0;
+$day = 0;
+$month = 0;
+//for ($time=)
+function isleapyear($year)
+{
+	return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
+}
+function monthdays($currentmonth,$currentyear)
+{
+	$bigmonths = array ("1","3","5","7","8","10","12");
+	$smallmonths = array ("4","6","9","11");
+	if (in_array($currentmonth,$bigmonths)){
+		$monthdays=31;
+	}
+	elseif (in_array($currentmonth,$smallmonths)){
+		$monthdays=30;
+	}
+	elseif ($currentmonth="2"){
+		$monthdays=29;
+		if (isleapyear($currentyear)){
+			$monthdays=28;
+		}
+	}
+	return $monthdays;
+}
+echo monthdays(2,2012);
+
+$timestamp = date("U", mktime(18, 55, 0, 8, 21, 2012));
+echo "<p>".$timestamp."</p>";
+//echo "August 21, 2012 is on a " . date("U", mktime(18, 55, 0, 8, 21, 2012));
+echo "<br></br>current timestamp is: ".$timestamp;
+echo "<br></br>current date is: ".date(DATE_RSS,$timestamp);
+echo "<br></br>current hour is: ".date("Gi",$timestamp)."\n\n";
+
+// Floating point random number function
+function fprand($intMin,$intMax,$intDecimals) {
+  if($intDecimals) {
+        $intPowerTen=pow(10,$intDecimals);
+        return rand($intMin,$intMax*$intPowerTen)/$intPowerTen;
+  }
+  else
+        return rand($intMin,$intMax);
+}
+
+
+// Example of fprand function
+for($i=0; $i<=5; $i++) {
+  echo "Three random numbers with $i decimals are: ";
+  for($ii=1; $ii<=3; $ii++)
+        echo fprand(1,10,$i).' ';
+  echo '<br>';
+}
+$factor = 1 + fprand(0,0.15,3);
+for ($i=0;$i<30;$i++){
+	$factor = 1+fprand(0,0.15,3);
+	echo $factor."<br/>";
+}
+
+?>
+</body>
+</html>
