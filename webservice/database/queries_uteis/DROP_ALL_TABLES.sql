64eea96c485cdb31fcf1a711faf1ff07a43a8bfb
cymbg1 <cymbaum@gmail.com>
2012-11-15 14:35:23 -0300

    Query para deletar todas as tabelas de TCC_GFX

---
+++ webservice/database/queries_uteis/DROP_ALL_TABLES.sql
@@ -0,0 +1,29 @@
+use TCC_GFX;
+
+## Remover Restrições
+
+ALTER TABLE medidas_dia 
+DROP FOREIGN KEY FK_collected_for;
+
+ALTER TABLE medidas_dia 
+DROP FOREIGN KEY FK_collected_at;
+
+ALTER TABLE medidas_mes 
+DROP FOREIGN KEY FK_gathered_for;
+
+ALTER TABLE medidas_mes 
+DROP FOREIGN KEY FK_gathered_at;
+
+#remover todas as tabelas
+
+DROP TABLE IF EXISTS medidas_mes ;
+
+DROP TABLE IF EXISTS medidas_dia;
+
+DROP TABLE IF EXISTS medidas;
+ 
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS residencias;
+
+DROP TABLE IF EXISTS feriados;
